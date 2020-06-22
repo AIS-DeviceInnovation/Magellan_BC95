d49817e9220e8591ef22aae5440010fc5ba998dc
AIS IoT Alliance Program <22192236+AIS-DeviceInnovation@users.noreply.github.com>
2020-06-22 09:24:44 +0700

    Create reportData.ino

---
+++ examples/reportData/reportData.ino
@@ -0,0 +1,42 @@
+/* 
+  Example for report random data to Magellan IoT Platform
+  please use payload with json format e.g. {"Temperature":25.5,"Humidity":90.4,"Light":45}
+  and {"Location":"13.001,100.44"} for plot the location on MAP
+                    |        |
+                latitude  longitude 
+
+  and {"Lamp":0} or {"Lamp":1"} for show the Lamp status on dashboard
+  
+  Please login and enjoy with https://magellan.ais.co.th
+  
+  If you have any questions, please see more details at https://magellan.ais.co.th
+  
+         " Plug NB-IoT Shield on Arduino Mega "
+    1. Jump Pin 8 (RX) NB to Pin 48 Mega 
+    2. Jump Pin 9 (TX) NB to Pin 46 Mega
+*/
+
+#include "Magellan_BC95.h"
+Magellan_BC95 magel;
+
+String payload;
+
+void setup() 
+{
+  Serial.begin(9600);
+  magel.begin();           //Init Magellan LIB
+}
+
+void loop() 
+{
+  /*
+    Example report random temperature and humidity to Magellan IoT platform
+  */
+  String temperature=String(random(0,100));
+  String humidity=String(random(0,100));
+  
+  payload="{\"temperature\":"+temperature+",\"humidity\":"+humidity+"}";  //Please provide payload with json format
+  
+  magel.report(payload);                                                  //Report random data to Magellan IoT platform
+  delay(5000);                                                            //Delay 5 second
+}
