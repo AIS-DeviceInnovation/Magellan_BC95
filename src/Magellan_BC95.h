6919552bcf26aeeae85cc74b48305be28bb8bd5f
AIS IoT Alliance Program <22192236+AIS-DeviceInnovation@users.noreply.github.com>
2023-02-21 10:01:23 +0700

    Add MQTT

--- src/Magellan_BC95.h
+++ src/Magellan_BC95.h
@@ -1,3 +1,4 @@
+
 /*
 Copyright (c) 2020, Advanced Wireless Network
 All rights reserved.
@@ -27,22 +28,22 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-Magellan_BC95 v2.0.0 NB-IoT Magellan Platform .
-support Quectel BC95-B8 & BC95-G
+Magellan_BC95 v2.1.0 NB-IoT Magellan Platform .
+support Quectel BC95 & BC95-G
 NB-IoT with AT command
 
-Library/SDK has developed with CoAP protocol. 
-reference with 
+Library/SDK has developed with CoAP protocol.
+reference with
 https://tools.ietf.org/html/rfc7252
 
 support post and get method
-and supported only Magellan IoT Platform 
+and supported only Magellan IoT Platform
 
-*** the payload has limit with the dynamic memory of your board   
- 
-Author: Device Innovation team     
-Create Date: 8 February 2021. 
-Modified: 14 June 2021.
+*** the payload has limit with the dynamic memory of your board
+
+Author: Device Innovation team
+Create Date: 8 February 2021.
+Modified: 17 February 2023.
 
 Released for private usage.
 */
@@ -50,154 +51,152 @@ Released for private usage.
 #ifndef Magellan_BC95_h
 #define Magellan_BC95_h
 
-#include <Arduino.h>
 #include "AT_BC95.h"
+#include <Arduino.h>
 
-  const byte maxretrans=4;	
+const byte maxretrans = 4;
 
-  //-------Msg Type------------
-  const char con[]="40";
-  const char contk[]="42";
-  const char non_con[]="50";
-  const char ack[]="60";
-  const char acktk[]="62";
-  const char rst[]="70";
+//-------Msg Type------------
+const char con[]     = "40";
+const char contk[]   = "42";
+const char non_con[] = "50";
+const char ack[]     = "60";
+const char acktk[]   = "62";
+const char rst[]     = "70";
 
-  //-------Method Type---------
-  //const char EMPTY[]="00";
-  const char GET[]="01";
-  const char POST[]="02";
-  const char PUT[]="03";
-  const char DELETE[]="04";
-
+//-------Method Type---------
+// const char EMPTY[]="00";
+const char GET[]    = "01";
+const char POST[]   = "02";
+const char PUT[]    = "03";
+const char DELETE[] = "04";
 
 enum rspcode {
-	EMPTY=00,
-	CREATED=65,
-	DELETED=66,
-	VALID=67,
-	CHANGED=68,
-	CONTENT=69,
-	CONTINUE=95,
-	BAD_REQUEST=128,
-	FORBIDDEN=131,
-	NOT_FOUND=132,
-	METHOD_NOT_ALLOWED=133,
-	NOT_ACCEPTABLE=134,
-	REQUEST_ENTITY_INCOMPLETE=136,
-	PRECONDITION_FAILED=140,
-	REQUEST_ENTITY_TOO_LARGE=141,
-	UNSUPPORTED_CONTENT_FORMAT=143,
-	INTERNAL_SERVER_ERROR=160,
-	NOT_IMPLEMENTED=161,
-	BAD_GATEWAY=162,
-	SERVICE_UNAVAILABLE=163,
-	GATEWAY_TIMEOUT=164,
-	PROXY_NOT_SUPPORTED=165
+  EMPTY                      = 00,
+  CREATED                    = 65,
+  DELETED                    = 66,
+  VALID                      = 67,
+  CHANGED                    = 68,
+  CONTENT                    = 69,
+  CONTINUE                   = 95,
+  BAD_REQUEST                = 128,
+  FORBIDDEN                  = 131,
+  NOT_FOUND                  = 132,
+  METHOD_NOT_ALLOWED         = 133,
+  NOT_ACCEPTABLE             = 134,
+  REQUEST_ENTITY_INCOMPLETE  = 136,
+  PRECONDITION_FAILED        = 140,
+  REQUEST_ENTITY_TOO_LARGE   = 141,
+  UNSUPPORTED_CONTENT_FORMAT = 143,
+  INTERNAL_SERVER_ERROR      = 160,
+  NOT_IMPLEMENTED            = 161,
+  BAD_GATEWAY                = 162,
+  SERVICE_UNAVAILABLE        = 163,
+  GATEWAY_TIMEOUT            = 164,
+  PROXY_NOT_SUPPORTED        = 165
 };
 
 typedef struct option {
-    String stroption;
-    unsigned int optlen;
-    unsigned int optionnum;
-} option; 
+  String       stroption;
+  unsigned int optlen;
+  unsigned int optionnum;
+} option;
 
-class Magellan_BC95
-{
-
+class Magellan_BC95 {
 
 public:
-	Magellan_BC95();
+  Magellan_BC95();
 
-	bool debug;
-	bool printstate=true;
-    //-------------------- Magellan Platform -------------------------
-    bool begin();
-    String getSignal();
-    pingRESP pingIP(String IP);
-    radio getRadioStat();
-    dateTime getClock(unsigned int timezone=7);
+  bool debug;
+  bool printstate = true;
+  //-------------------- Magellan Platform -------------------------
+  bool     begin();
+  void     powerSavingMode(unsigned int psm, String Requested_PeriodicTAU = "",
+                           String Requested_Active_Time = "");
+  String   getSignal();
+  pingRESP pingIP(String IP);
+  radio    getRadioStat();
+  dateTime getClock(unsigned int timezone = 7);
 
-    String thingsRegister();
-    String report(String payload,unsigned int qos=0);
-    String getConfig(String Resource,unsigned int qos=0);
-    String getControl(String Resource,unsigned int qos=0); 
+  String thingsRegister();
+  String report(String payload, unsigned int qos = 0);
+  String getConfig(String Resource, unsigned int qos = 0);
+  String getControl(String Resource, unsigned int qos = 0);
 
 private:
+  unsigned int Msg_ID = 0;
+  //------------------------- Buffer --------------------------
+  String data_input = "";
+  String rcvdata    = "";
+  String imei       = "";
+  String imsi       = "";
+  String iccid      = "";
+  String deviceIP;
+  String model;
+  String data_buffer = "";
+  String data_resp   = "";
+  String Token       = "";
+  //--------------------- counter value ---------------------
+  byte k = 0;
+  //------------------------- flag --------------------------
+  bool breboot_flag       = false;
+  bool end                = false;
+  bool flag_rcv           = true;
+  bool en_get             = true;
+  bool en_post            = true;
+  bool en_send            = true;
+  bool getpayload         = false;
+  bool sendget            = false;
+  bool NOTFOUND           = false;
+  bool GETCONTENT         = false;
+  bool RCVRSP             = false;
+  bool success            = false;
+  bool connected          = false;
+  bool get_process        = false;
+  bool post_process       = false;
+  bool ACK                = false;
+  bool EMP                = false;
+  bool token_error_report = true;
+  bool token_error_config = true;
+  //------------- message token and ID --------------
+  unsigned int resp_msgID = 0;
+  unsigned int post_ID    = 0;
+  unsigned int post_token = 0;
+  unsigned int get_ID     = 0;
+  unsigned int get_token  = 0;
+  //-------------------- timer ----------------------
+  unsigned int previous_send          = 0;
+  unsigned int previous_get           = 0;
+  unsigned int token                  = 0;
+  unsigned int rsptoken               = 0;
+  byte         count_post_timeout     = 0;
+  byte         count_get_timeout      = 0;
+  byte         count_error_token_post = 0;
+  byte         count_error_token_get  = 0;
 
-	unsigned int Msg_ID=0;
-	//------------------------- Buffer --------------------------
-	String data_input="";
-	String rcvdata="";
-	String imei="";
-	String imsi="";
-	String iccid="";
-	String deviceIP;
-	String model;
-	String data_buffer="";
-	String data_resp="";
-	String Token="";
-	//--------------------- counter value ---------------------
-	byte k=0;
-	//------------------------- flag --------------------------
-	bool breboot_flag=false;
-	bool end=false;
-	bool flag_rcv=true;
-	bool en_get=true;
-	bool en_post=true;
-	bool en_send=true;
-	bool getpayload=false;
-	bool sendget=false;
-	bool NOTFOUND=false;
-	bool GETCONTENT=false;
-	bool RCVRSP=false;
-	bool success=false;
-	bool connected=false;
-	bool get_process=false;
-	bool post_process=false;
-	bool ACK=false;
-	bool EMP=false;
-	bool token_error_report=true;
-	bool token_error_config=true;
-	//------------- message token and ID --------------
-	unsigned int resp_msgID=0;
-	unsigned int post_ID=0;
-	unsigned int post_token=0;
-	unsigned int get_ID=0;
-	unsigned int get_token=0;
-	//-------------------- timer ----------------------
-	unsigned int previous_send=0;
-	unsigned int previous_get=0;
-	unsigned int token=0;
-	unsigned int rsptoken=0;
-	byte count_post_timeout=0;
-	byte count_get_timeout=0;
-	byte count_error_token_post=0;
-	byte count_error_token_get=0;
+  //------------- Diagnostic & Report ---------------
+  byte cnt_cmdgetrsp = 0;
+  //---------------------- Function -------------------
+  void printHEX(char *str);
+  void printMsgID(unsigned int messageID);
+  void printRspHeader(String Msgstr);
+  void printRspType(String Msgstr, unsigned int msgID);
+  void printPathlen(unsigned int path_len, String init_str);
+  void printUriPath(String uripath, String optnum);
+  void printErrCode(String errCode);
 
-	//------------- Diagnostic & Report ---------------
-	byte cnt_cmdgetrsp=0;
-	//---------------------- Function -------------------
-    void printHEX(char *str);
-    void printMsgID(unsigned int messageID);
-	void printRspHeader(String Msgstr);
-	void printRspType(String Msgstr,unsigned int msgID);
-	void printPathlen(unsigned int path_len,String init_str);
-	void printUriPath(String uripath,String optnum);
-	void printErrCode(String errCode);
+  //---------------------- Response Management --------
+  void manageResponse(String rx);
+  void rspPrintOut(String rx);
 
-	//---------------------- Response Management --------
-	void manageResponse(String rx);
-	void rspPrintOut(String rx);
-
-	//--------------------- Message management ----------
-	void msgPost(String payload,option *coapOption,unsigned int totaloption);
-	void msgGet(option *coapOption,unsigned int totaloption,String Proxy);
-	String postData(String payload,option *coapOption,unsigned int totaloption);
-	String getData(option *coapoption,unsigned int totaloption,String Proxy);
+  //--------------------- Message management ----------
+  void   msgPost(String payload, option *coapOption, unsigned int totaloption);
+  void   msgGet(option *coapOption, unsigned int totaloption, String Proxy);
+  String postData(String payload, option *coapOption, unsigned int totaloption);
+  String getData(option *coapoption, unsigned int totaloption, String Proxy);
 
 protected:
-	 Stream *_Serial;
+  Stream *_Serial;
 };
 
 #endif
